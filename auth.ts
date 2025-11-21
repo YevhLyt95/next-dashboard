a7118286f270496d25e3d36d1bfdaec2f126c5b9
Hunter1995 <zenyalyt@gmail.com>
2025-11-21 14:49:15 +0100

    Logout and some bug fixes

--- auth.ts
+++ auth.ts
@@ -6,11 +6,11 @@ import type { User } from '@/app/lib/definitions';
 import bcrypt from 'bcrypt';
 import postgres from 'postgres';
 
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+const sql = postgres(process.env.DATABASE_URL!);
 
 async function getUser(email: string): Promise<User | undefined> {
     try {
-        const user = await sql<User[]>`SELECT * FROM users WHERE email=${email}`;
+        const user = await sql<User[]>`SELECT * FROM "User" WHERE email=${email}`;
         return user[0];
     } catch (error) {
         console.error('Failed to fetch user:', error);
